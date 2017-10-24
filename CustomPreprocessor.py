b1d23cf0d014be67144b28eee9b241bf8315f4e2
Theodore Norvell <theo@mun.ca>
2017-10-24 14:30:07 -0230

    Added custom preprocessor

---
+++ CustomPreprocessor.py
@@ -0,0 +1,13 @@
+from nbconvert.preprocessors import Preprocessor
+import re
+
+# Adapted from https://stackoverflow.com/questions/42255564/how-to-export-and-preserve-linked-jupyter-notebooks/42279992#42279992
+class CustomPreprocessor(Preprocessor):
+
+    def preprocess_cell(self, cell, resources, index):
+        if 'source' in cell and cell.cell_type == "markdown":
+            print( "BEFORE:" + cell.source )
+            cell.source = re.sub(r"\[([^]]*)\]\(([^)]*)\.ipynb\)",r"[\1](\2.html)",cell.source)
+            print( "AFTER:" + cell.source )
+
+        return cell, resources
