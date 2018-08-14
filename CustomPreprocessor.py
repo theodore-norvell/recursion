cdc111eca578e8d5e1f1f6d263bdbb8450870ba7
Theodore Norvell <theo@mun.ca>
2018-08-14 14:54:30 -0230

    Corrected licence file.

--- CustomPreprocessor.py
+++ CustomPreprocessor.py
@@ -7,6 +7,7 @@ class CustomPreprocessor(Preprocessor):
     def preprocess_cell(self, cell, resources, index):
         if 'source' in cell and cell.cell_type == "markdown":
             #print( "BEFORE:" + cell.source )
+            # Replace any links to Jupyter files with links to html files.
             cell.source = re.sub(r"\[([^]]*)\]\(([^)]*)\.ipynb\)",r"[\1](\2.html)",cell.source)
             #print( "AFTER:" + cell.source )
 
